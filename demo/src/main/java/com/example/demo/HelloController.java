97c2ecec5492f07bb48af59c085534e73fc52b6c
Nirari <nirarib@gmail.com>
2026-03-06 22:29:50 -0600

    Feat: Add User entity, UserRepository, update HelloController and configure H2 for embedded database. Removed data.sql. Fixes #1

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final UserRepository userRepository;
+
+    public HelloController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/")
     public String hello() {
         return "Hello, Spring Boot with H2 Database!";
+    }
+
+    @GetMapping("/users")
+    public String getUsers() {
+        StringBuilder sb = new StringBuilder();
+        for (User user : userRepository.findAll()) {
+            sb.append(user.toString()).append("\n");
+        }
+        return sb.toString();
     }
 }
